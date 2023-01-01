a5eba899ac9bfa15f7b4fc50d38ab80114cd433b
Sebastian <sebastian4.3.96@gmx.de>
2023-01-01 18:42:56 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# TODO: change user password
+
+
+# upgrade all packages
+sudo apt-get update
+sudo apt-get upgrade
+
+# TODO: install raspotify
+# Bitrate 320
+# Login via /etc/default/raspotify
+
+# TODO: install hifiberry sound card
+# TODO: ALSAMIXER VOLUME TO 100
+
+# TODO: install homeassistant via docker
+
+# TODO: install pihole
