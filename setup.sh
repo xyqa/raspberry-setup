6758405c6e0f5dff2bc9e5304178368c0b7f4f92
Sebastian <sebastian4.3.96@gmx.de>
2023-01-04 22:11:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,23 @@
-# TODO: change user password
-
-
 # upgrade all packages
 sudo apt-get update
 sudo apt-get upgrade
 
+# Install raspotify: https://github.com/dtcooper/raspotify
+sudo apt-get -y install curl && curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
+
+# Optionally Install higher quality sample rate converters
 # TODO: install raspotify
 # Bitrate 320
 # Login via /etc/default/raspotify
+
+sudo apt update && sudo apt install -y asound-conf-wizard
+
+# Copy raspotify config file
+sudo systemctl restart raspotify
+
+
+
+
 
 # TODO: install hifiberry sound card
 # TODO: ALSAMIXER VOLUME TO 100
