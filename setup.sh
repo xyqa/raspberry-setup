31b8923ebc3f2e099966b4efc41c8e05d8d0630f
Sebastian <sebastian4.3.96@gmx.de>
2023-01-11 21:25:07 +0100

    Spotifyd finished

--- setup.sh
+++ setup.sh
@@ -7,40 +7,13 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Install Hifiberry Sound card
-# ---------------------------
-## Disable driver for onboard sound
-sudo sed -i 's/dtparam=audio=on/#dtparam=audio=on/' /boot/config.txt
+# ---------------------------<
+## Disable driver for onboard sound & enable Hifiberry Soundcard
+sudo sed -i 's/dtparam=audio=on/#dtparam=audio=on\ndtoverlay=hifiberry-dacplus/' /boot/config.txt
 
 ## TODO: Chose sound card as main output device
 
-
-## Enable Hifiberry Soundcard
-echo "\n[CUSTOM]\ndtoverlay=hifiberry-dacplus\n" | sudo tee -a /boot/config.txt
-
-
-# Install spotifyd: https://github.com/Spotifyd/spotifyd
-## Download & unzip
-XYQA_SPOTIFYD_PATH="/home/$XYQA_USER_NAME/spotifyd"
-
-mkdir $XYQA_SPOTIFYD_PATH
-
-wget https://github.com/Spotifyd/spotifyd/releases/download/v0.3.4/spotifyd-linux-armhf-default.tar.gz -P $XYQA_SPOTIFYD_PATH
-tar xzf spotifyd-linux-armhf-default.tar.gz
-
-## Copy config file
-mkdir ~/.config/spotifyd
-cp $XYQA_RASPBERRY_SETUP_PATH/config/spotifyd.conf ~/.config/spotifyd/spotifyd.conf
-
-## Systemd daemon file
-sudo wget https://raw.githubusercontent.com/Spotifyd/spotifyd/master/contrib/spotifyd.service -O /etc/systemd/user/spotifyd.service
-sudo sed -i "s|ExecStart\=.*|ExecStart=$XYQA_SPOTIFYD_PATH/spotifyd --no-daemon|" /etc/systemd/user/spotifyd.service
-
-## Starting spotifyd at boot
-sudo loginctl enable-linger $XYQA_USER_NAME
-systemctl --user enable spotifyd.service
-
-
-# TODO: ALSAMIXER VOLUME TO 100
+source setup_spotifyd.sh
 
 # TODO: install homeassistant via docker
 
---
+++ setup_spotifyd.sh
@@ -0,0 +1,22 @@
+# Install spotifyd: https://github.com/Spotifyd/spotifyd
+## Download & unzip
+XYQA_SPOTIFYD_PATH="/home/$XYQA_USER_NAME/spotifyd"
+
+mkdir $XYQA_SPOTIFYD_PATH
+
+wget https://github.com/Spotifyd/spotifyd/releases/download/v0.3.4/spotifyd-linux-armhf-default.tar.gz -P $XYQA_SPOTIFYD_PATH
+tar xzf $XYQA_SPOTIFYD_PATH/spotifyd-linux-armhf-default.tar.gz -C $XYQA_SPOTIFYD_PATH
+
+## Copy config file
+mkdir ~/.config/spotifyd
+cp $XYQA_RASPBERRY_SETUP_PATH/config/spotifyd.conf ~/.config/spotifyd/spotifyd.conf
+
+## Systemd daemon file
+mkdir -p ~/.config/systemd/user/
+sudo wget https://raw.githubusercontent.com/Spotifyd/spotifyd/master/contrib/spotifyd.service -O ~/.config/systemd/user/spotifyd.service
+sudo sed -i "s|ExecStart\=.*|ExecStart=$XYQA_SPOTIFYD_PATH/spotifyd/spotifyd --no-daemon|" ~/.config/systemd/user/spotifyd.service
+systemctl --user daemon-reload
+
+## Starting spotifyd at boot
+sudo loginctl enable-linger $XYQA_USER_NAME
+systemctl --user enable spotifyd.service
